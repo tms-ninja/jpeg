4de61314e1d217ceb4cb6110afd3289b668366bf
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2025-01-08 18:35:41 +0000

    Add Array_2d
    - Add Array_2d class
    - Add default construction to Array
    - Add resize() method to Array
    - Add tests

--- include/jpeg_cpp/array.h
+++ include/jpeg_cpp/array.h
@@ -1,6 +1,7 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <array>
 #include <iostream>
 #include <vector>
 
@@ -15,6 +16,9 @@ namespace JPEG {
         std::vector<T> buffer;
 
     public:
+        /// @brief Creates an empty Array
+        Array() {}
+
         /// @brief Creates an Array of the given size
         /// @param size Number of elements to create array with
         Array(size_t size) : buffer(size) {}
@@ -37,6 +41,13 @@ namespace JPEG {
         /// @return A pointer to the underlying buffer
         const T* data() const { return buffer.data(); }
 
+        /// @brief Resizes the array
+        /// @param size New size of the array
+        void resize(size_t size)
+        {
+            buffer.resize(size);
+        }
+
         T& operator[](size_t ind) { return buffer[ind] ;}
         const T& operator[](size_t ind) const { return buffer[ind] ;}
 
@@ -47,6 +58,130 @@ namespace JPEG {
                 out << elem << ' ';
             }
 
+            return out;
+        }
+    };
+
+    /// @brief A 2d array stored as a contiguous array in row-major order
+    /// @tparam T Element type
+    template<typename T>
+    class Array_2d
+    {
+    private:
+        Array<T> buffer;
+        std::array<size_t, 2> arr_shape;
+
+    public:
+        /// @brief Creates an Array_2d with the given number of rows and columns
+        /// @param rows 
+        /// @param columns 
+        Array_2d(size_t rows, size_t columns)
+        : buffer(rows*columns), arr_shape{rows, columns}
+        {
+
+        }
+
+        Array_2d(const std::initializer_list<std::initializer_list<T>>& ls)
+        {
+            // For now, assume neither number of rows or columns is zero
+            arr_shape[0] = ls.size();
+            arr_shape[1] = (*ls.begin()).size();
+
+            buffer.resize(arr_shape[0]*arr_shape[1]);
+
+            size_t i{ 0 }, j{ 0 };
+
+            for (auto& row: ls)
+            {
+                // Verify the row size
+
+                // remember to reset j
+                j = 0;
+                
+                for (auto& elem: row)
+                {
+                    at(i, j) = elem;
+                    j++;
+                }
+
+                i++;
+            }
+        }
+
+        /// @brief Shape of the array
+        /// @return Shape of the array as rows/columns
+        std::array<size_t, 2> shape() const { return arr_shape; }
+
+        /// @brief Number of elements in the Array_2d
+        /// @return Number of elements in the Array_2d
+        size_t size() const { return buffer.size(); }
+
+        /// @brief Returns a pointer to the underlying buffer
+        /// @return A pointer to the underlying buffer
+        const T* data() const { return buffer.data(); }
+
+        /// @brief Access elements as a 1d array
+        /// @param ind Index of element as a 1d array
+        /// @return Element at index ind
+        T& operator[](size_t ind) { return buffer[ind] ;}
+
+        /// @brief Access elements as a 1d array
+        /// @param ind Index of element as a 1d array
+        /// @return Element at index ind
+        const T& operator[](size_t ind) const { return buffer[ind] ;}
+
+        /// @brief Gets the element at the given row and column
+        /// @param row The row of the desired element
+        /// @param column The column of the desired element
+        /// @return Reference to the element at the given position
+        T& at(size_t row, size_t column)
+        {
+            size_t ind{ row*arr_shape[1] + column };
+
+            return buffer[ind];
+        }
+        
+        /// @brief Gets the element at the given row and column
+        /// @param row The row of the desired element
+        /// @param column The column of the desired element
+        /// @return Reference to the element at the given position
+        const T& at(size_t row, size_t column) const
+        {
+            size_t ind{ row*arr_shape[1] + column };
+
+            return buffer[ind];
+        }
+
+        /// @brief Gets the element at the given row and column
+        /// @param row The row of the desired element
+        /// @param column The column of the desired element
+        /// @return Reference to the element at the given position
+        T& operator()(size_t row, size_t column)
+        {
+            return at(row, column);
+        }
+
+        /// @brief Gets the element at the given row and column
+        /// @param row The row of the desired element
+        /// @param column The column of the desired element
+        /// @return Reference to the element at the given position
+        const T& operator()(size_t row, size_t column) const
+        {
+            return at(row, column);
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, const Array_2d<T>& arr)
+        {
+            for (size_t i=0; i<arr.arr_shape[0]; i++)
+            {
+                for (size_t j=0; j<arr.arr_shape[1]-1; j++)
+                {
+                    out << at(i, j) << ' ';
+                }
+                // Don't forget the last element in the row
+                out << at(i, arr.arr_shape[1]-1) << '\n';
+            }
+            
             return out;
         }
     };
--- test/jpeg_cpp_test/array_test.cpp
+++ test/jpeg_cpp_test/array_test.cpp
@@ -26,3 +26,103 @@ TEST_CASE( "Array::construction of Arrays", "[Array]" ) {
         }
     }
 }
+
+TEST_CASE( "Array::resizing of Arrays", "[Array]" ) {
+    Array<int> arr_actual = {0, 1, 2, 3, 4, 5};
+    size_t initial_size{ 5 };
+
+    SECTION( "can be resized larger" ) {
+        size_t new_size{ 10 };
+
+        arr_actual.resize(new_size);
+
+        // Check the new size
+        REQUIRE( arr_actual.size() == new_size );
+
+        // Check initial elements are preserved
+        for (size_t ind = 0; ind < initial_size; ind++)
+        {
+            REQUIRE( arr_actual[ind] == ind );
+        }
+    }
+    SECTION( "can be resized smaller" ) {
+        size_t new_size{ 3 };
+
+        arr_actual.resize(new_size);
+
+        // Check the new size
+        REQUIRE( arr_actual.size() == new_size );
+
+        // Check initial elements are preserved
+        for (size_t ind = 0; ind < new_size; ind++)
+        {
+            REQUIRE( arr_actual[ind] == ind );
+        }
+    }
+    SECTION( "can be resized the same" ) {
+        size_t new_size{ initial_size };
+
+        arr_actual.resize(new_size);
+
+        // Check the new size
+        REQUIRE( arr_actual.size() == new_size );
+
+        // Check initial elements are preserved
+        for (size_t ind = 0; ind < new_size; ind++)
+        {
+            REQUIRE( arr_actual[ind] == ind );
+        }
+    }
+}
+
+TEST_CASE( "Array_2d::construction of Array_2ds", "[Array_2d]" ) {
+
+    SECTION( "constructing using number of rows and columns" ) {
+        size_t expected_rows{ 2 }, expected_cols{ 3 };
+        Array_2d<int> arr_actual{ 2, 3 };
+
+        REQUIRE( arr_actual.size() == expected_rows*expected_cols );
+        REQUIRE( arr_actual.shape()[0] == expected_rows );
+        REQUIRE( arr_actual.shape()[1] == expected_cols );
+    }
+    SECTION( "constructing using initializer list" ) {
+        size_t expected_rows{ 2 }, expected_cols{ 3 };
+        Array_2d<int> arr_actual = {
+            {0, 1, 2},
+            {3, 4, 5}
+        };
+
+        REQUIRE( arr_actual.size() == expected_rows*expected_cols );
+        REQUIRE( arr_actual.shape()[0] == expected_rows );
+        REQUIRE( arr_actual.shape()[1] == expected_cols );
+
+        // Check values of elements
+        for (size_t i = 0; i < expected_rows; i++)
+        {
+            for (size_t j = 0; j < expected_cols; j++)
+            {
+                REQUIRE( arr_actual.at(i, j) == i*expected_cols + j );
+            }
+        }
+    }
+}
+
+TEST_CASE( "Array_2d::2d subscripting of Array_2ds", "[Array_2d]" ) {
+    size_t expected_rows{ 2 }, expected_cols{ 3 };
+    Array_2d<int> arr_actual{ 2, 3 };
+
+    for (int i = 0; i < arr_actual.size(); i++)
+    {
+        arr_actual[i] = i;
+    }
+
+    // Now test each by accessing them in row major order
+    for (size_t i = 0; i < expected_rows; i++)
+    {
+        for (size_t j = 0; j < expected_cols; j++)
+        {
+            REQUIRE( arr_actual.at(i, j) == i*expected_cols + j );
+        }
+    }
+}
+
