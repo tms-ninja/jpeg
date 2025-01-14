d66ec00a18d1d603796b08288251ee11b3a6977d
tms-ninja <65960785+tms-ninja@users.noreply.github.com>
2025-01-14 15:23:49 +0000

    Add DCT functions

--- include/jpeg_cpp/encoding.h
+++ include/jpeg_cpp/encoding.h
@@ -1,11 +1,39 @@
 #ifndef ENCODING_H
 #define ENCODING_H
 
+#include <array>
+
+// Needed to access pi constant
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 #include "jpeg_cpp/array.h"
 
 namespace JPEG
 {
     void apply_level_shift(DU_Array<double>& array);
+
+    /// @brief Loads matrix for computing the discrete cosine transform
+    /// This is defined as M_ij = 1/2 cos((2*i+1)*j*pi/16)
+    /// @return DCT matrix
+    Array_2d<double> load_DCT_matrix();
+
+    /// @brief Loads the transpose of the DCT matrix
+    /// @return Transpose of the DCT matrix
+    Array_2d<double> load_DCT_matrix_transpose();
+
+    const static Array_2d<double> DCT_matrix{ load_DCT_matrix() };
+    const static Array_2d<double> DCT_matrix_transpose{ load_DCT_matrix_transpose() };
+
+    /// @brief Computes the matrix multiplication c = a @ b. It assumes matrices are square
+    /// and are stored in row-major order.
+    /// @param a 
+    /// @param b 
+    /// @param c 
+    /// @param N Size of the matrix, ie. a, b c have shape (N, N)
+    void mat_mul(const double* a, const double* b, double* c, size_t N);
+
+    void apply_DCT(DU_Array<double>& array);
 }
 
 #endif
--- src/jpeg_cpp/encoding.cpp
+++ src/jpeg_cpp/encoding.cpp
@@ -9,4 +9,74 @@ namespace JPEG
             array[ind] -= 128.0;
         }
     }
+
+    Array_2d<double> load_DCT_matrix()
+    {
+        Array_2d<double> dct_matrix{ 8, 8 };
+
+        for (size_t i = 0; i < dct_matrix.shape()[0]; i++)
+        {
+            for (size_t j = 0; j < dct_matrix.shape()[1]; j++)
+            {
+                dct_matrix(i, j) = std::cos((2*i+1)*j*M_PI / 16.0) / 2.0;
+            }
+        }
+        
+        return dct_matrix;
+    }
+
+    Array_2d<double> load_DCT_matrix_transpose()
+    {
+        Array_2d<double> dct_matrix{ load_DCT_matrix() };
+
+        // Compute the transpose manually
+        for (size_t i = 0; i < dct_matrix.shape()[0]; i++)
+        {
+            for (size_t j = 0; j < i; j++)
+            {
+                double old_value_at_ij{ dct_matrix(i, j) };
+
+                dct_matrix(i, j) = dct_matrix(j, i);
+                dct_matrix(j, i) = old_value_at_ij;
+            }
+        }
+
+        return dct_matrix;
+    }
+
+    void mat_mul(const double* a, const double* b, double* c, size_t N)
+    {
+        for (size_t i = 0; i < N; i++)
+        {
+            for (size_t j = 0; j < N; j++)
+            {
+                // Compute c_ij = sum_k a_ik*b_kj
+                double c_ij{ 0.0 };
+
+                for (size_t k = 0; k < N; k++)
+                {
+                    c_ij += a[i*N+k] * b[k*N+j];
+                }
+                
+                // Don't forget to set c_ij!
+                c[i*N + j] = c_ij;
+            }
+        }
+    }
+
+    void apply_DCT(DU_Array<double> &array)
+    {
+        // Buffer to store the intermediate result of data unit*DCT matrix
+        std::array<double, 64> double_buffer;
+        const auto arr_shape{ array.shape() };
+
+        for (size_t du_ind = 0; du_ind < arr_shape[0]; du_ind++)
+        {
+            // Compute data unit*DCT matrix
+            mat_mul(&array[du_ind*64], DCT_matrix.data(), &double_buffer[0], arr_shape[1]);
+
+            // Compute transpose(DCT matrix) * termporary
+            mat_mul(DCT_matrix_transpose.data(), &double_buffer[0], &array[du_ind*64], arr_shape[1]);
+        }
+    }
 }
